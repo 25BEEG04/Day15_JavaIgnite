65b8d3b299c906962847d398cb91ea76d439a725
25BEEG04 <abhijitbiswal700@gmail.com>
2026-06-14 19:33:26 +0530

    Update Classroom_attendance_system.java

--- Multithreading/Classroom_attendance_system.java
+++ Multithreading/Classroom_attendance_system.java
@@ -1,91 +1,67 @@
-/*
-Classroom Attendance System
+package JavaIgniteDay15;
 
-Teacher thread:
+class Classroom {
+	boolean attendancestarted = false;
 
-Takes attendance
+	synchronized void waitForAttendance() {
+		while(attendancestarted == false) {
+			System.out.println("Students are waiting");
+			try {
+				wait();
+			}
+			catch (Exception e) {
+				System.out.println("Error handeled");
+			}
+		}
+		System.out.println("Students marked present !!");
+	}
+	synchronized void startAttendance() {
+		attendancestarted = true;
+		System.out.println("Teacher started attendance ");
+		notifyAll();
+	}
+}
+class Teacherthread extends Thread {
+	Classroom classroom;
+	Teacherthread(Classroom classroom) {
+		this.classroom = classroom;
+	}
+	public void run() {
+		classroom.startAttendance();
+	}
+}
+class Studentthread extends Thread {
+	Classroom classroom;
+	Studentthread(Classroom classroom) {
+		this.classroom = classroom;
+	}
+	 public void run() {
+	        classroom.waitForAttendance();
+	    }
+}
+public class Classroom_attendance_system {
 
-Student thread:
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Classroom classroom = new Classroom();
+		Studentthread s1 = new Studentthread(classroom);
+		Studentthread s2 = new Studentthread(classroom);
+		Studentthread s3 = new Studentthread(classroom);
+		Studentthread s4 = new Studentthread(classroom);
+		
+		Teacherthread teacher = new Teacherthread(classroom);
+		s1.start();
+		s2.start();
+		s3.start();
+		s4.start();
+		
+		try {
+			Thread.sleep(3000);
+		} 
+		catch (Exception e) {
+			System.out.println("Eerror Handled");
+		}
+		teacher.start();
+	}
 
-Waits until attendance starts
-Requirements
-Students should not proceed immediately.
-Students must wait.
-Teacher gives signal.
-Students continue after signal.
-
-Expected Flow:
-
-Student Waiting...
-
-Teacher Started Attendance
-
-Student Marked Present
-Concepts Tested
-wait()
-notifyAll()
-
-
-Instructions - 
-Create Class Classroom
-
-    Variable:
-        attendanceStarted = false
-
-    synchronized method waitForAttendance()
-
-        While attendanceStarted is false
-
-            Print:
-                "Student Waiting..."
-
-            wait()
-
-        Print:
-            "Student Marked Present"
-
-
-    synchronized method startAttendance()
-
-        attendanceStarted = true
-
-        Print:
-            "Teacher Started Attendance"
-
-        notifyAll()
-
-
-Create Class TeacherThread
-
-    Classroom classroom
-
-    run()
-
-        classroom.startAttendance()
-
-
-Create Class StudentThread
-
-    Classroom classroom
-
-    run()
-
-        classroom.waitForAttendance()
-
-
-Main Method
-
-    Create Classroom object
-
-    Create multiple Student threads
-
-    Create Teacher thread
-
-    Start Student threads
-
-    Wait for a few seconds
-
-    Start Teacher thread
-Thread Coordination
-Synchronization
-*/
+}
